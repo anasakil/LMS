c34c2663c94b88b6448647ec40f8eabfe48969b4
anasakil <96956171+anasakil@users.noreply.github.com>
2025-04-09 17:12:47 +0100

    Update footer.blade.php

--- resources/views/web/default/includes/footer.blade.php
+++ resources/views/web/default/includes/footer.blade.php
@@ -1,33 +1,21 @@
-@php
-    $socials = getSocials();
-    if (!empty($socials) and count($socials)) {
-        $socials = collect($socials)->sortBy('order')->toArray();
-    }
-
-    $footerColumns = getFooterColumns();
-@endphp
-
 <footer class="footer bg-secondary position-relative user-select-none">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class=" footer-subscribe d-block d-md-flex align-items-center justify-content-between">
+                <div class="footer-subscribe d-block d-md-flex align-items-center justify-content-between">
                     <div class="flex-grow-1">
-                        <strong>{{ trans('footer.join_us_today') }}</strong>
-                        <span class="d-block mt-5 text-white">{{ trans('footer.subscribe_content') }}</span>
+                        <strong>Rejoignez-nous aujourd'hui</strong>
+                        <span class="d-block mt-5 text-white">Inscrivez-vous à notre newsletter pour recevoir les dernières nouvelles et mises à jour</span>
                     </div>
                     <div class="subscribe-input bg-white p-10 flex-grow-1 mt-30 mt-md-0">
                         <form action="/newsletters" method="post">
-                            {{ csrf_field() }}
+                            <input type="hidden" name="_token" value="CSRF_TOKEN">
 
                             <div class="form-group d-flex align-items-center m-0">
                                 <div class="w-100">
-                                    <input type="text" name="newsletter_email" class="form-control border-0 @error('newsletter_email') is-invalid @enderror" placeholder="{{ trans('footer.enter_email_here') }}"/>
-                                    @error('newsletter_email')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
+                                    <input type="text" name="newsletter_email" class="form-control border-0" placeholder="Entrez votre adresse e-mail ici"/>
                                 </div>
-                                <button type="submit" class="btn btn-primary rounded-pill">{{ trans('footer.join') }}</button>
+                                <button type="submit" class="btn btn-primary rounded-pill">Rejoindre</button>
                             </div>
                         </form>
                     </div>
@@ -36,33 +24,58 @@
         </div>
     </div>
 
-    @php
-        $columns = ['first_column','second_column','third_column','forth_column'];
-    @endphp
-
     <div class="container">
         <div class="row">
-
-            @foreach($columns as $column)
-                <div class="col-6 col-md-3">
-                    @if(!empty($footerColumns[$column]))
-                        @if(!empty($footerColumns[$column]['title']))
-                            <span class="header d-block text-white font-weight-bold">{{ $footerColumns[$column]['title'] }}</span>
-                        @endif
-
-                        @if(!empty($footerColumns[$column]['value']))
-                            <div class="mt-20">
-                                {!! $footerColumns[$column]['value'] !!}
-                            </div>
-                        @endif
-                    @endif
+            <!-- Première colonne -->
+            <div class="col-6 col-md-3">
+                <span class="header d-block text-white font-weight-bold">À propos d'Algorithmics</span>
+                <div class="mt-20">
+                    <p class="text-white">Algorithmics est un centre de formation spécialisé dans l'enseignement de la programmation pour les enfants. Nous aidons les jeunes à développer leurs compétences numériques et à se préparer pour l'avenir.</p>
                 </div>
-            @endforeach
+            </div>
 
+            <!-- Deuxième colonne -->
+            <div class="col-6 col-md-3">
+                <span class="header d-block text-white font-weight-bold">Nos programmes</span>
+                <div class="mt-20">
+                    <ul class="footer-menu">
+                        <li><a href="/programmes/scratch">Scratch pour débutants</a></li>
+                        <li><a href="/programmes/python">Python Junior</a></li>
+                        <li><a href="/programmes/web">Développement Web</a></li>
+                        <li><a href="/programmes/robotique">Robotique</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Troisième colonne -->
+            <div class="col-6 col-md-3">
+                <span class="header d-block text-white font-weight-bold">Infos pratiques</span>
+                <div class="mt-20">
+                    <ul class="footer-menu">
+                        <li><a href="/horaires">Horaires des cours</a></li>
+                        <li><a href="/tarifs">Tarifs</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                        <li><a href="/contact">Nous contacter</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Quatrième colonne -->
+            <div class="col-6 col-md-3">
+                <span class="header d-block text-white font-weight-bold">Événements</span>
+                <div class="mt-20">
+                    <ul class="footer-menu">
+                        <li><a href="/ateliers">Ateliers découverte</a></li>
+                        <li><a href="/camps">Camps de vacances</a></li>
+                        <li><a href="/competitions">Compétitions</a></li>
+                        <li><a href="/blog">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
         <div class="mt-40 border-blue py-25 d-flex align-items-center justify-content-between">
-            <div class="footer-logo">
+        <div class="footer-logo">
                 <a href="/">
                     @if(!empty($generalSettings['footer_logo']))
                         <img src="{{ $generalSettings['footer_logo'] }}" class="img-cover" alt="footer logo">
@@ -107,5 +120,4 @@
             </div>
         </div>
     @endif
-
 </footer>
